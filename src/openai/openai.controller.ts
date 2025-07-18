6a70500e7ce1f6c8add17eefeff52baa384fd9ad
Sneha Pal <Sneha1289pal@gmail.com>
2025-07-18 13:31:38 +0530

    Update OpenAI controller and service to improve chatbot functionality

--- src/openai/openai.controller.ts
+++ src/openai/openai.controller.ts
@@ -1,8 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { createChatCompletionRequest } from './dto/create-chat-completion.request';
+import { OpenaiService } from './openai.service';
 
 @Controller('openai')
 export class OpenaiController {
+  constructor(private readonly openaiService: OpenaiService) {}
   @Post('chatCompletion')
-  async createChatCompletion(@Body() body: createChatCompletionRequest) {}
+  async createChatCompletion(@Body() body: createChatCompletionRequest) {
+    return this.openaiService.createChatCompletion(body.messages);
+  }
 }
--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
+import { ChatCompletionMessageParam } from 'openai/resources/index';
+import { ChatCompletionMessageDto } from './dto/create-chat-completion.request';
 
 @Injectable()
-export class OpenaiService {}
+export class OpenaiService {
+  constructor(private readonly openai: OpenAI) {}
+
+  async createChatCompletion(messages: ChatCompletionMessageDto[]) {
+    return this.openai.chat.completions.create({
+      messages: messages as ChatCompletionMessageParam[],
+      model: 'gpt-4',
+    });
+  }
+}
